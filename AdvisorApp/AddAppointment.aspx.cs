e500983c5ef8604acd625edc996570630f25d727
SamP2324 <35580208+SamP2324@users.noreply.github.com>
2018-08-01 18:39:19 -0500

    worked on adding an appointment

--- AdvisorApp/AddAppointment.aspx.cs
+++ AdvisorApp/AddAppointment.aspx.cs
@@ -18,20 +18,26 @@ public partial class AddAppointment : System.Web.UI.Page
         AppointmentAppDbEntities dbcon = new AppointmentAppDbEntities();
         dbcon.AppointmentTables.Load();
 
-        AppointmentTable myAppointment = new AppointmentTable();
-        myAppointment.AdvisorName = txtAdvisorName.Text;
-        myAppointment.Location = txtLocation.Text;
-        myAppointment.Date = Convert.ToDateTime(lblDate.Text);
         int hour = Convert.ToInt32(ddlHour.SelectedValue.ToString());
         int minute = Convert.ToInt32(ddlMinute.SelectedValue.ToString());
-        myAppointment.Time = new TimeSpan(hour, minute, 0);
-        myAppointment.Reason = txtReason.Text;
 
         if (validateAppointment(txtAdvisorName.Text, Convert.ToDateTime(lblDate.Text), new TimeSpan(hour, minute, 0)))
         {
+            AppointmentTable myAppointment = new AppointmentTable();
+            myAppointment.AdvisorName = txtAdvisorName.Text;
+            myAppointment.Location = txtLocation.Text;
+            myAppointment.Date = Convert.ToDateTime(lblDate.Text);
+            myAppointment.Time = new TimeSpan(hour, minute, 0);
+            myAppointment.Reason = txtReason.Text;
             dbcon.AppointmentTables.Add(myAppointment);
             dbcon.SaveChanges();
             gvBrowse.DataBind();
+            lstResult.Items.Clear();
+            lstResult.Items.Add("Student name: name, Advisor Name: " + txtAdvisorName.Text);
+            lstResult.Items.Add("Location: " + txtLocation.Text);
+            lstResult.Items.Add("Date: " + Convert.ToDateTime(lblDate.Text));
+            lstResult.Items.Add("Time: " + new TimeSpan(hour, minute, 0));
+            lstResult.Items.Add("Reason: " + txtReason.Text);
         }
     }
 
@@ -56,17 +62,20 @@ public partial class AddAppointment : System.Web.UI.Page
 
     private bool advisorAvailable(string advisorName, DateTime appDate, TimeSpan appTime)
     {
-        
+        AdvisorDBEntities dbcon = new AdvisorDBEntities();
+        dbcon.AdvisorTables.Load();
+
         var existingApp =
-            from x in dbcon.AppointmentTables.Local
+            from x in dbcon.AdvisorTables.Local
             where x.Date.Equals(appDate) && x.Time.Equals(appTime)
             select x;
         if (existingApp != null)
         {
             lstResult.Items.Clear();
-            lstResult.Items.Add("You already have an existing appointment at that date and time.");
+            lstResult.Items.Add("The advisor is unavailable at the selected date and time.");
             return false;
         }
+        return true;
     }
 
     protected void Calendar1_SelectionChanged(object sender, EventArgs e)
